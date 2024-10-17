20cd4d9754670f5211f3eb9142e05256f17a0a1b
m1rrozZ <m1rrozov@gmail.com>
2024-10-17 13:15:59 +0300

    5 lab

---
+++ Programming/lab05/lab05/lab05.cpp
@@ -0,0 +1,145 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+class Multiplication
+{
+public:
+    void Mult()
+    {
+        cout << "Перевiримо табличку множення! \n"
+            "P.S. - щоб закiнчити программу введiть </>"
+            << endl;
+
+        while (true)
+        {
+            int rand_num1 = rand() % 10;
+            int rand_num2 = rand() % 10;
+            int correct_answer = rand_num1 * rand_num2;
+            string exit_p;
+
+            while (true)
+            {
+                cout << "Скiльки буде.. " << rand_num1 << " * " << rand_num2 << " = " << endl;
+                cin >> exit_p;
+
+                if (exit_p == "</>")
+                {
+                    return;
+                }
+
+                try
+                {
+                    int answer = atoi(exit_p.c_str()); // Преобразование ответа в число
+
+                    if (answer == correct_answer)
+                    {
+                        int rand_num = rand() % 4;
+                        switch (rand_num)
+                        {
+                        case 0:
+                            cout << "Дуже добре!" << endl;
+                            break;
+                        case 1:
+                            cout << "Вiдмiнно!" << endl;
+                            break;
+                        case 2:
+                            cout << "Чудова робота!" << endl;
+                            break;
+                        case 3:
+                            cout << "Продовжуйте працювати так само добре!" << endl;
+                            break;
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        int rand_num = rand() % 4;
+                        switch (rand_num)
+                        {
+                        case 0:
+                            cout << "Нi, спробуйте ще раз." << endl;
+                            break;
+                        case 1:
+                            cout << "Невiрно, спробуйте ще раз." << endl;
+                            break;
+                        case 2:
+                            cout << "Не опускайте руки!" << endl;
+                            break;
+                        case 3:
+                            cout << "Нi, продовжуйте вашi спроби." << endl;
+                            break;
+                        }
+
+                        break;
+
+                    }
+                }
+                catch (const exception& e)
+                {
+                    cerr << "Помилка: " << e.what() << '\n';
+                }
+            }
+        }
+    };
+};
+
+class Task3
+{
+    private:
+        float x1, x2, y1, y2;
+
+    public:
+        inline float distance()
+        {
+            while (true)
+            {
+                string input;
+
+                cin.ignore();
+
+                cout << "Введiть значення для x2: " << endl;
+                getline(cin, input);
+                x2 = (input.empty()) ? 0 : stof(input);
+
+                cout << "Введiть значення для x1:" << endl;
+                getline(cin, input);
+                x1 = (input.empty()) ? 0 : stof(input);
+
+                cout << "Введiть значення для y2: " << endl;
+                getline(cin, input);
+                y2 = (input.empty()) ? 0 : stof(input);
+
+                cout << "Введiть значення для y1:" << endl;
+                getline(cin, input);
+                y1 = (input.empty()) ? 0 : stof(input);
+
+                break;
+            }
+
+            
+            float res_D = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+            cout << "Result: " << res_D << endl;
+            return res_D;
+        }
+
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Ukrainian");
+    srand(time(0));
+
+    cout << "Start program." << endl;
+
+    Multiplication multiplication;
+    multiplication.Mult();
+
+    Task3 task;
+    task.distance();
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,37 +1,37 @@
 ### Programowania
 
-## Programwania (Progress [4 / 13])
- ***lab-01 - 1;
- ***lab-02 - 1;
- ***lab-03 - 1;
- ***lab-04 - 1;
- ***lab-05 - 0;
- ***lab-06 - 0;
- ***lab-07(MKR) - 0;
- ***lab-08 - 0;
- ***lab-09 - 0;
- ***lab-10 - 0
- ***lab-11 - 0;
- ***lab-12(MKR) - 0;
- ***lab-13 - 0;
+## Programowania (Progress [4 / 13])
+
+ * **lab-01 - 1;
+ * **lab-02 - 1;
+ * **lab-03 - 1;
+ * **lab-04 - 1;
+ * **lab-06 - 0;
+ * **lab-07(MKR) - 0;
+ * **lab-08 - 0;
+ * **lab-09 - 0;
+ * **lab-10 - 0
+ * **lab-11 - 0;
+ * **lab-12(MKR) - 0;
+ * **lab-13 - 0;
 
 ## Project's programming (Progress [2 / 6])
- ***tasks 1/2 - 1;
- ***tasks 3/4 - 1;
- ***tasks 5/6 - 0;
- ***tasks 7/8 - 0;
- ***tasks 9 - 0;
- ***project-c++ - 0
+  * **tasks 1/2 - 1;
+  * **tasks 3/4 - 1;
+  * **tasks 5/6 - 0;
+  * **tasks 7/8 - 0;
+  * **tasks 9 - 0;
+  * **project-c++ - 0
 
 ## Asemb (Progress [0 / 11])
- ***lab-01 - 0;
- ***lab-02 - 0;
- ***lab-03 - 0;
- ***lab-04 - 0;
- ***lab-05 - 0;
- ***lab-06 - 0;
- ***lab-07 - 0;
- ***lab-08 - 0;
- ***lab-09 - 0;
- ***lab-10 - 0;
- ***lab-11 - 0;
+ * **lab-01 - 0;
+ * **lab-02 - 0;
+ * **lab-03 - 0;
+ * **lab-04 - 0;
+ * **lab-05 - 0;
+ * **lab-06 - 0;
+ * **lab-07 - 0;
+ * **lab-08 - 0;
+ * **lab-09 - 0;
+ * **lab-10 - 0;
+ * **lab-11 - 0;
